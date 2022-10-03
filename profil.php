a0264d300fc923fdc173679140c7668f0abeac29
zakoz <80521058+MPanina@users.noreply.github.com>
2022-10-04 00:56:32 +0300

    Add files via upload

--- profil.php
+++ profil.php
@@ -77,6 +77,7 @@ include 'connect.php';
                         echo "<p class='sit__paragraph'>" . "Номер места: " . "$sitRow[sit_num]" . "</p>";
                         echo "<p class='sit__paragraph'>" . "Цена места: " . "$sitRow[sit_price] " . "&#8381" . "</p>";
                         echo "<p class='sit__paragraph'>" . "Расположение места: " . "$sitRow[sit_direction]" . "</p>";
+                        /*echo "<a href='theatre.php?posterid=" . $sitRow['id_poster'] . "&placeid=" . $sitRow['id'] . "' class='sit__link'>" . "[Cнять бронь]" . "</a>";*/
 
                         echo "</li>";
                     }
